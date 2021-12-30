33c812d7b3c7a9228f06bf27590f4a44a6f5b4a1
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-30 14:36:35 +0100

    auto-type-By-MONSTERMC-SRC
    auto-type-By-MONSTERMC-SRC

---
+++ auto type/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Ambiance_ThemeContainer1 = New WindowsApplication1.Ambiance_ThemeContainer()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Ambiance_Button_12 = New WindowsApplication1.Ambiance_Button_1()
+        Me.Ambiance_Label1 = New WindowsApplication1.Ambiance_Label()
+        Me.TIM = New WindowsApplication1.Ambiance_TextBox()
+        Me.T1 = New WindowsApplication1.Ambiance_TextBox()
+        Me.Ambiance_Button_21 = New WindowsApplication1.Ambiance_Button_2()
+        Me.Ambiance_Button_11 = New WindowsApplication1.Ambiance_Button_1()
+        Me.Ambiance_ThemeContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'Ambiance_ThemeContainer1
+        '
+        Me.Ambiance_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(CType(CType(244, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.Ambiance_ThemeContainer1.Controls.Add(Me.LinkLabel1)
+        Me.Ambiance_ThemeContainer1.Controls.Add(Me.Ambiance_Button_12)
+        Me.Ambiance_ThemeContainer1.Controls.Add(Me.Ambiance_Label1)
+        Me.Ambiance_ThemeContainer1.Controls.Add(Me.TIM)
+        Me.Ambiance_ThemeContainer1.Controls.Add(Me.T1)
+        Me.Ambiance_ThemeContainer1.Controls.Add(Me.Ambiance_Button_21)
+        Me.Ambiance_ThemeContainer1.Controls.Add(Me.Ambiance_Button_11)
+        Me.Ambiance_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Ambiance_ThemeContainer1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.Ambiance_ThemeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.Ambiance_ThemeContainer1.Name = "Ambiance_ThemeContainer1"
+        Me.Ambiance_ThemeContainer1.Padding = New System.Windows.Forms.Padding(20, 56, 20, 16)
+        Me.Ambiance_ThemeContainer1.RoundCorners = True
+        Me.Ambiance_ThemeContainer1.Sizable = False
+        Me.Ambiance_ThemeContainer1.Size = New System.Drawing.Size(306, 401)
+        Me.Ambiance_ThemeContainer1.SmartBounds = True
+        Me.Ambiance_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Ambiance_ThemeContainer1.TabIndex = 0
+        Me.Ambiance_ThemeContainer1.Text = "Auto Typer Spamm"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.Location = New System.Drawing.Point(204, 372)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(86, 14)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "MONSTERMC"
+        '
+        'Ambiance_Button_12
+        '
+        Me.Ambiance_Button_12.BackColor = System.Drawing.Color.Transparent
+        Me.Ambiance_Button_12.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.Ambiance_Button_12.Image = Nothing
+        Me.Ambiance_Button_12.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Ambiance_Button_12.Location = New System.Drawing.Point(210, 309)
+        Me.Ambiance_Button_12.Name = "Ambiance_Button_12"
+        Me.Ambiance_Button_12.Size = New System.Drawing.Size(73, 55)
+        Me.Ambiance_Button_12.TabIndex = 5
+        Me.Ambiance_Button_12.Text = "Exit"
+        Me.Ambiance_Button_12.TextAlignment = System.Drawing.StringAlignment.Center
+        '
+        'Ambiance_Label1
+        '
+        Me.Ambiance_Label1.AutoSize = True
+        Me.Ambiance_Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Ambiance_Label1.Font = New System.Drawing.Font("Segoe UI", 11.0!)
+        Me.Ambiance_Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(76, Byte), Integer), CType(CType(76, Byte), Integer), CType(CType(77, Byte), Integer))
+        Me.Ambiance_Label1.Location = New System.Drawing.Point(222, 210)
+        Me.Ambiance_Label1.Name = "Ambiance_Label1"
+        Me.Ambiance_Label1.Size = New System.Drawing.Size(61, 20)
+        Me.Ambiance_Label1.TabIndex = 4
+        Me.Ambiance_Label1.Text = ".Second"
+        '
+        'TIM
+        '
+        Me.TIM.BackColor = System.Drawing.Color.Transparent
+        Me.TIM.Font = New System.Drawing.Font("Tahoma", 11.0!)
+        Me.TIM.ForeColor = System.Drawing.Color.FromArgb(CType(CType(76, Byte), Integer), CType(CType(76, Byte), Integer), CType(CType(76, Byte), Integer))
+        Me.TIM.Location = New System.Drawing.Point(23, 205)
+        Me.TIM.MaxLength = 32767
+        Me.TIM.Multiline = False
+        Me.TIM.Name = "TIM"
+        Me.TIM.ReadOnly = False
+        Me.TIM.Size = New System.Drawing.Size(193, 28)
+        Me.TIM.TabIndex = 3
+        Me.TIM.Text = "2"
+        Me.TIM.TextAlignment = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TIM.UseSystemPasswordChar = False
+        '
+        'T1
+        '
+        Me.T1.BackColor = System.Drawing.Color.Transparent
+        Me.T1.Font = New System.Drawing.Font("Tahoma", 11.0!)
+        Me.T1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(76, Byte), Integer), CType(CType(76, Byte), Integer), CType(CType(76, Byte), Integer))
+        Me.T1.Location = New System.Drawing.Point(23, 59)
+        Me.T1.MaxLength = 32767
+        Me.T1.Multiline = True
+        Me.T1.Name = "T1"
+        Me.T1.ReadOnly = False
+        Me.T1.Size = New System.Drawing.Size(260, 140)
+        Me.T1.TabIndex = 2
+        Me.T1.Text = "Your text here"
+        Me.T1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.T1.UseSystemPasswordChar = False
+        '
+        'Ambiance_Button_21
+        '
+        Me.Ambiance_Button_21.BackColor = System.Drawing.Color.Transparent
+        Me.Ambiance_Button_21.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Ambiance_Button_21.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Bold)
+        Me.Ambiance_Button_21.Image = Nothing
+        Me.Ambiance_Button_21.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Ambiance_Button_21.Location = New System.Drawing.Point(23, 246)
+        Me.Ambiance_Button_21.Name = "Ambiance_Button_21"
+        Me.Ambiance_Button_21.Size = New System.Drawing.Size(260, 55)
+        Me.Ambiance_Button_21.TabIndex = 1
+        Me.Ambiance_Button_21.Text = "Start Typing"
+        Me.Ambiance_Button_21.TextAlignment = System.Drawing.StringAlignment.Center
+        '
+        'Ambiance_Button_11
+        '
+        Me.Ambiance_Button_11.BackColor = System.Drawing.Color.Transparent
+        Me.Ambiance_Button_11.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Ambiance_Button_11.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.Ambiance_Button_11.Image = Nothing
+        Me.Ambiance_Button_11.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Ambiance_Button_11.Location = New System.Drawing.Point(23, 309)
+        Me.Ambiance_Button_11.Name = "Ambiance_Button_11"
+        Me.Ambiance_Button_11.Size = New System.Drawing.Size(181, 55)
+        Me.Ambiance_Button_11.TabIndex = 0
+        Me.Ambiance_Button_11.Text = "Stop Typing"
+        Me.Ambiance_Button_11.TextAlignment = System.Drawing.StringAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(306, 401)
+        Me.Controls.Add(Me.Ambiance_ThemeContainer1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MinimumSize = New System.Drawing.Size(261, 65)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Auto Typer Spamm"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.Ambiance_ThemeContainer1.ResumeLayout(False)
+        Me.Ambiance_ThemeContainer1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Ambiance_ThemeContainer1 As WindowsApplication1.Ambiance_ThemeContainer
+    Friend WithEvents Ambiance_Label1 As WindowsApplication1.Ambiance_Label
+    Friend WithEvents TIM As WindowsApplication1.Ambiance_TextBox
+    Friend WithEvents T1 As WindowsApplication1.Ambiance_TextBox
+    Friend WithEvents Ambiance_Button_21 As WindowsApplication1.Ambiance_Button_2
+    Friend WithEvents Ambiance_Button_11 As WindowsApplication1.Ambiance_Button_1
+    Friend WithEvents Ambiance_Button_12 As WindowsApplication1.Ambiance_Button_1
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ auto type/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        SendKeys.Send(T1.Text)
+        SendKeys.Send("{Enter}")
+    End Sub
+
+    Private Sub Ambiance_Button_21_Click(sender As Object, e As EventArgs) Handles Ambiance_Button_21.Click
+        Timer1.Interval = TIM.Text * 1000
+        Timer1.Start()
+        Ambiance_Button_21.Enabled = False
+        Ambiance_Button_11.Enabled = True
+    End Sub
+
+    Private Sub Ambiance_Button_11_Click(sender As Object, e As EventArgs) Handles Ambiance_Button_11.Click
+        Timer1.Stop()
+        Ambiance_Button_21.Enabled = True
+        Ambiance_Button_11.Enabled = False
+    End Sub
+
+    Private Sub Ambiance_Button_12_Click(sender As Object, e As EventArgs) Handles Ambiance_Button_12.Click
+        End
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://www.youtube.com/channel/UCLF-eRNc52VslhdctpHaAzg")
+    End Sub
+End Class
